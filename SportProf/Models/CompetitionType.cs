9d1c492d14b3c2e397c8e8c2c51b112e5331c554
jack1799 <jack1799@mail.ru>
2019-11-18 17:48:21 +0200

    change

--- SportProf/Models/CompetitionType.cs
+++ SportProf/Models/CompetitionType.cs
@@ -3,7 +3,6 @@
     public class CompetitionType
     {
         public int Id { get; set; }
-
         public string Name { get; set; } = "";
         public string Descryption { get; set; } = "";
     }
